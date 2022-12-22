00e994b86fbb5c0f134e4abb2a84a4fb3ffcd762
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2022-12-22 09:52:01 +0700

    Install TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./renderer/pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

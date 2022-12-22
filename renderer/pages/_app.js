e524bca053975c13f322ad8a5c22fba38af12ac9
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2022-12-22 10:16:01 +0700

    Styling page

--- renderer/pages/_app.js
+++ renderer/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
